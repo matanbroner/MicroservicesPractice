9994b6939172b72ac3f2c962d754915e9d54c18d
Matan Broner <matanbroner@gmail.com>
2019-03-25 22:12:32 -0700

    Added user service

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,15 +1,30 @@
 # services/users/project/__init__.py
 
+import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-# instantiate the app
-app = Flask(__name__)
 
-app.config.from_object('project.config.DevelopmentConfig')
+# instantiate the db
+db = SQLAlchemy()
 
-@app.route('/users/ping', methods=['GET']) 
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+def create_app(script_info=None):
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    db.init_app(app)
+
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+    
+    return app
+
